e2054e7eb68ff8bbc116547b2ee806a1bdb48455
Chase Malik <chase.malik@duke.edu>
2018-12-09 02:01:41 -0800

    Add other images

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import React, { Component } from 'react';
 
 import CustomLightbox from './components/CustomLightbox';
 import Header from './components/Header';
-import { HOPE, PEACE } from './constants/images';
+import { ALL_IMAGES } from './constants/images';
 
 const theme = createMuiTheme({
   palette: {
@@ -17,32 +17,22 @@ const theme = createMuiTheme({
 
 class App extends Component {
   render() {
+    const imageLinks = ALL_IMAGES.map((images) => {
+      return (
+        <Grid item xs={12} md={6} lg={4}>
+          <CustomLightbox images={images} />
+        </Grid>
+      );
+    })
     return (
       <MuiThemeProvider theme={theme}>
         <header>
           <Header />
         </header>
         <main className="app-container" style={{ padding: 12 }}>
-          <Grid container spacing={24} alignItems="center" justify="center">
-            <Grid item xs={12} md={6} lg={4}>
-              {/* <Grid item xs={12} container justify="center"> */}
-              <CustomLightbox images={HOPE} />
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <CustomLightbox images={PEACE} />
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <CustomLightbox images={HOPE} />
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <CustomLightbox images={PEACE} />
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <CustomLightbox images={HOPE} />
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <CustomLightbox images={PEACE} />
-            </Grid>
+          <Grid container spacing={24} alignItems="center">
+            {/* <Grid item xs={12} container justify="center"> */}
+            {imageLinks}
           </Grid>
         </main>
       </MuiThemeProvider>
--- src/constants/images.ts
+++ src/constants/images.ts
@@ -6,5 +6,10 @@ const getImagesForRange = (folder: string, count: number) => {
         .map(src => ({ src }));
 }
 
-export const HOPE = getImagesForRange('2008-hope', 14);
+export const CHEER = getImagesForRange('2016-cheer', 13);
+export const WONDER = getImagesForRange('2014-wonder', 13);
+export const JOY = getImagesForRange('2012-joy', 14);
 export const PEACE = getImagesForRange('2010-peace', 14);
+export const HOPE = getImagesForRange('2008-hope', 14);
+
+export const ALL_IMAGES = [CHEER, WONDER, JOY, PEACE, HOPE];
